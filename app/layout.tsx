134775b64fb9867ce98d0cf14ff5cf8bfcb86fcf
ikomavinayamato <di22226666ikoma@gmail.com>
2025-02-02 01:37:41 +0900

    fix: scale

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,6 +42,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja">
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+      </head>
       <body className={inter.className}>
         <AuthProvider>{children}</AuthProvider>
       </body>
